1098041515b9a9bf576888d4eea0aedbcd4eedf7
semermaks <semermaks@gmail.com>
2024-07-11 10:23:15 +0300

    Format fix

--- jobs_python/spiders/jobs.py
+++ jobs_python/spiders/jobs.py
@@ -12,7 +12,10 @@ technologies = os.getenv("TECHNOLOGIES").split(",")
 class JobsSpider(scrapy.Spider):
     name = "jobs"
     allowed_domains = ["www.work.ua"]
-    start_urls = ["https://www.work.ua/jobs-python/", "https://www.work.ua/jobs-python-програміст/"]
+    start_urls = [
+        "https://www.work.ua/jobs-python/",
+        "https://www.work.ua/jobs-python-програміст/",
+    ]
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -31,22 +34,17 @@ class JobsSpider(scrapy.Spider):
             return ""
 
     def parse(self, response: Response, **kwargs):
-        for job in response.css('.my-0>a'):
-            job_url = response.urljoin(job.css('::attr(href)').get())
+        for job in response.css(".my-0>a"):
+            job_url = response.urljoin(job.css("::attr(href)").get())
             description = self._parse_jobs_details(job_url).lower()
 
             tech_found = {}
             for tech in technologies:
                 tech_found[tech] = description.find(tech.lower()) != -1
 
-            yield {
-                "title": job.css('::attr(title)').get(),
-                **tech_found
-            }
+            yield {"title": job.css("::attr(title)").get(), **tech_found}
         next_page = response.css(".pagination>li")[-1].css("a::attr(href)").get()
         print(next_page)
         if next_page is not None:
             next_page_url = response.urljoin(next_page)
             yield scrapy.Request(next_page_url, callback=self.parse, dont_filter=True)
-
-
